ef1434090374acbccdf7f01d2361ed974dbd1b5f
unknown <sahu.shibashis07@gmail.com>
2025-08-11 23:56:43 +0530

    hosted on firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyANUcAPUo3qM8bVcq5OhMrQXfBiTzQfu9w",
+    authDomain: "time-logger-f3c05.firebaseapp.com",
+    projectId: "time-logger-f3c05",
+    storageBucket: "time-logger-f3c05.firebasestorage.app",
+    messagingSenderId: "1098783480541",
+    appId: "1:1098783480541:web:e86c94ae01f4eadf021934",
+    measurementId: "G-ZN8Z73PJCJ"
 };
 
 const app = initializeApp(firebaseConfig);
